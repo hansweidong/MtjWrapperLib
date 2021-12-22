b17f09e3d9c27d23995fca30b9a4e00d5e6912de
hansweidong <wwdhao163@163.com>
2021-12-22 16:13:33 +0800

    commit code

--- mtjwrapperlib/src/main/java/com/baidu/mobstat/IIgnoreAutoEvent.java
+++ mtjwrapperlib/src/main/java/com/baidu/mobstat/IIgnoreAutoEvent.java
@@ -1,6 +1,9 @@
 package com.baidu.mobstat;
 
 /**
+ *
+ * IIgnoreAutoEvent 实现该接口的activity 将不会被mtj库监控，不持有activity下的view
+ *
  * @Author wuweidong
  * @Date 2021/12/22-14:41
  * @Email wuweidong@yy.com
